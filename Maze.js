3361eaa5b3636e9b5377c6cdbda0f4bb4b3aecb2
HessLincolnCS <hesslincolncs1@gmail.com>
2025-01-28 07:42:21 -0500

    Green Checkpoint

--- Maze.js
+++ Maze.js
@@ -21,8 +21,8 @@ await speak("Start", true);
 //Driver: Lincoln Hess
 //Navigator: Lucas Winfrey
 
-await setSpeed(60);
-	await delay(2.65);
+await setSpeed(30);
+	await delay(5.2);
 	await setSpeed(0);
 	await delay(1);
 	await setMainLed({ r: 0, g: 255, b: 0 });
